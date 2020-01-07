f3060f62be8a995f7e5d29c946cfaaf766c21abe
WangZhongQi <657704095@qq.com>
2020-01-07 18:06:19 +0800

    CreditValueDao测试类，测试成功 基础方法测试成功，还要去业务类拼接修改和添加功能

--- manage/provider/e-user-provider/src/test/java/com/example/euserprovider/CreditValueDaoTest.java
+++ manage/provider/e-user-provider/src/test/java/com/example/euserprovider/CreditValueDaoTest.java
@@ -1,8 +1,18 @@
 package com.example.euserprovider;
 
+import com.example.emodeldao.user.po.TCreditKey;
+import com.example.emodeldao.user.po.TCreditValue;
+import com.example.emodeldao.user.po.User;
+import com.example.euserprovider.dao.CreditValueDao;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 类描述:
@@ -13,4 +23,104 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class CreditValueDaoTest {
+
+    @Resource
+    private CreditValueDao creditValueDao;
+
+    @Test
+    public void findByCreditIdOrUserId(){
+        Map<String,Object> map = new HashMap<>();
+        TCreditValue creditValue = new TCreditValue();
+
+        TCreditKey creditKey = new TCreditKey();
+        creditKey.setId(1);
+        creditValue.setTCreditId(creditKey);
+
+        User user = new User();
+        user.setId(1);
+        creditValue.setUserId(user);
+
+        map.put("TCreditValue",creditValue);
+
+        map.put("page",0);
+        map.put("length",10);
+
+        List<TCreditValue> creditValues = creditValueDao.findByCreditIdOrUserId(map);
+
+        for(int i=0;i<creditValues.size();i++){
+            System.out.println(creditValues.get(i).getUserId().getUserNickName());
+            System.out.println(creditValues.get(i).getTCreditId().getKey());
+            System.out.println(creditValues.get(i).getValue());
+        }
+    }
+
+    @Test
+    public void insert(){
+        System.out.println();
+
+        Map<String,Object> map = new HashMap<>();
+        TCreditValue creditValue = new TCreditValue();
+
+        TCreditKey creditKey = new TCreditKey();
+        creditKey.setId(1);
+        creditValue.setTCreditId(creditKey);
+
+        User user = new User();
+        user.setId(1);
+        creditValue.setUserId(user);
+
+
+        creditValue.setValue("95959");
+        creditValue.setCreationTime("2020-1-7 17:57");
+        creditValue.setModifyId("1");
+
+
+        map.put("TCreditValue",creditValue);
+
+        creditValueDao.insert(map);
+    }
+
+    @Test
+    public void update(){
+        System.out.println();
+
+        System.out.println();
+
+        Map<String,Object> map = new HashMap<>();
+        TCreditValue creditValue = new TCreditValue();
+
+        TCreditKey creditKey = new TCreditKey();
+        creditKey.setId(1);
+        creditValue.setTCreditId(creditKey);
+
+        User user = new User();
+        user.setId(1);
+        creditValue.setUserId(user);
+
+
+        creditValue.setValue("15");
+        creditValue.setCreationTime("2020-1-7 17:57");
+        creditValue.setModifyTime("2020-1-7 18:03");
+        creditValue.setModifyId("1");
+
+        creditValue.setId(4);
+
+        map.put("TCreditValue",creditValue);
+
+
+        creditValueDao.update(map);
+    }
+
+    @Test
+    public void del(){
+        System.out.println();
+        Map<String,Object> map = new HashMap<>();
+        TCreditValue creditValue = new TCreditValue();
+
+        creditValue.setId(1);
+        creditValue.setDel(1);
+        map.put("TCreditValue",creditValue);
+
+        creditValueDao.del(map);
+    }
 }
