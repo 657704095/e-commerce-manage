8cd41e186bd0e48aec97037d4f1df89a03e62a4c
WangZhongQi <657704095@qq.com>
2020-01-07 11:16:32 +0800

    mybaits 延迟加载测试成功

--- manage/e-model/src/main/java/com/example/emodeldao/user/po/TCreditValue.java
+++ manage/e-model/src/main/java/com/example/emodeldao/user/po/TCreditValue.java
@@ -1,6 +1,7 @@
 package com.example.emodeldao.user.po;
 
 import com.example.emodeldao.annotation_validator.MyId;
+import com.example.emodeldao.fatherPublic.po.FatherAndDel;
 import lombok.Data;
 
 import javax.validation.constraints.Size;
@@ -12,10 +13,13 @@ import javax.validation.constraints.Size;
  * @Date: 2019/12/16 0016 16:21
  */
 @Data
-public class TCreditValue {
+public class TCreditValue extends FatherAndDel {
 
     private TCreditKey tCreditId;
 
     @Size(max = 20,message = "信用的值最大不能大于20位")
     private String value;
+
+    private User userId;
+
 }
