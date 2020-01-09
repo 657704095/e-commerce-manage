94827a31c1873104b06d28915638df0c868525ad
WangZhongQi <657704095@qq.com>
2020-01-09 15:14:36 +0800

    框架搭建成功

--- manage/provider/e-merchant-provider/e-commodity-provider/src/main/java/com/example/ecommodityprovider/ECommodityProviderApplication.java
+++ manage/provider/e-merchant-provider/e-commodity-provider/src/main/java/com/example/ecommodityprovider/ECommodityProviderApplication.java
@@ -1,11 +1,13 @@
 package com.example.ecommodityprovider;
 
+import com.didispace.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2Doc
 public class ECommodityProviderApplication {
 
     public static void main(String[] args) {
