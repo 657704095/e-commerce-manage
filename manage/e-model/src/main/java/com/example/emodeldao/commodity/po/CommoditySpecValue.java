2fb215ad2d84b9b92bcc3996a1bb475447b81cc3
WangZhongQi <657704095@qq.com>
2020-01-11 13:50:20 +0800

    CommoditySpecValueDao,基本dao写完

--- manage/e-model/src/main/java/com/example/emodeldao/commodity/po/CommoditySpecValue.java
+++ manage/e-model/src/main/java/com/example/emodeldao/commodity/po/CommoditySpecValue.java
@@ -1,6 +1,7 @@
 package com.example.emodeldao.commodity.po;
 
 import com.example.emodeldao.fatherPublic.po.Father2;
+import com.example.emodeldao.fatherPublic.po.Father2AndDel;
 import lombok.Data;
 
 import javax.validation.constraints.Size;
@@ -12,7 +13,7 @@ import javax.validation.constraints.Size;
  * @Date: 2019/12/16 0016 17:45
  */
 @Data
-public class CommoditySpecValue extends Father2 {
+public class CommoditySpecValue extends Father2AndDel {
 
     private CommoditySpec specId;
 
