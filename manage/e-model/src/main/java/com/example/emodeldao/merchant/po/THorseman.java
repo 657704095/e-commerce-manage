176a4768e819ef67545f7c7c2a52c858116fa66f
WangZhongQi <657704095@qq.com>
2020-01-14 18:41:42 +0800

    补充了一些字段

--- manage/e-model/src/main/java/com/example/emodeldao/merchant/po/THorseman.java
+++ manage/e-model/src/main/java/com/example/emodeldao/merchant/po/THorseman.java
@@ -1,6 +1,7 @@
 package com.example.emodeldao.merchant.po;
 
 import com.example.emodeldao.fatherPublic.po.Father;
+import com.example.emodeldao.fatherPublic.po.FatherAndDel;
 import lombok.Data;
 
 import javax.validation.constraints.Pattern;
@@ -13,7 +14,7 @@ import javax.validation.constraints.Size;
  * @Date: 2019/12/16 0016 16:39
  */
 @Data
-public class THorseman extends Father {
+public class THorseman extends FatherAndDel {
 
     @Size(max = 10,message = "骑手姓名不能大于10位")
     private String name;
@@ -21,4 +22,11 @@ public class THorseman extends Father {
     @Size(max = 18,message = "电话不能大于18位")
     @Pattern(regexp = "^[0-9]*$",message = "只能输入数字")
     private String tel;
+
+    @Size(max = 30,message = "用户省份证长度不能大于30位")
+    @Pattern(regexp = "^[A-Za-z0-9]+$",message = "只能如输入数字和字母")
+    private String IDCard;
+
+    @Size(max = 10,message = "安全行为数不能大于10位")
+    private String safeBehaviorNumber;
 }
