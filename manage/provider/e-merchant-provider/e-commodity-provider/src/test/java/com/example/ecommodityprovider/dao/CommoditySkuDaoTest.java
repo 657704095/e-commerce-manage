36a082f9353de8ec3e94677687b7328b15b54c9a
WangZhongQi <657704095@qq.com>
2020-01-16 17:03:22 +0800

    CommoditySkuDaoTest，测试类测试完毕。

--- manage/provider/e-merchant-provider/e-commodity-provider/src/test/java/com/example/ecommodityprovider/dao/CommoditySkuDaoTest.java
+++ manage/provider/e-merchant-provider/e-commodity-provider/src/test/java/com/example/ecommodityprovider/dao/CommoditySkuDaoTest.java
@@ -1,12 +1,17 @@
 package com.example.ecommodityprovider.dao;
 
 import com.example.emodeldao.commodity.po.CommoditySku;
+import com.example.emodeldao.commodity.po.CommoditySpu;
+import com.example.emodeldao.merchant.po.TMerchant;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 类描述:
@@ -23,7 +28,26 @@ public class CommoditySkuDaoTest {
 
     @Test
     public void find(){
+        Map<String,Object> map = new HashMap<>();
+        CommoditySku commoditySku = new CommoditySku();
+        //commoditySku.setSkuNo(15615);
+        //commoditySku.setSkuName("15");
 
+        CommoditySpu commoditySpu = new CommoditySpu();
+        commoditySpu.setId(1);
+        commoditySku.setSpuId(commoditySpu);
+
+        TMerchant tMerchant = new TMerchant();
+        tMerchant.setId(1);
+        //commoditySku.setMerchantId(tMerchant);
+
+        map.put("CommoditySku",commoditySku);
+
+        map.put("page",0);
+        map.put("length",10);
+        List<CommoditySku> commoditySkus = commoditySkuDao.find(map);
+
+        System.out.println(commoditySkus.get(0).getSkuName());
     }
 
     @Test
@@ -34,16 +58,36 @@ public class CommoditySkuDaoTest {
 
     @Test
     public void insert(){
+        Map<String,Object> map = new HashMap<>();
+        CommoditySku commoditySku = commoditySkuDao.findById(1);
 
+        commoditySku.setSkuNo(151651514);
+
+        map.put("CommoditySku",commoditySku);
+
+        commoditySkuDao.insert(map);
     }
 
     @Test
     public void update(){
+        Map<String,Object> map = new HashMap<>();
+        CommoditySku commoditySku = commoditySkuDao.findById(1);
 
+        commoditySku.setSkuName("小优游戏卡");
+
+        map.put("CommoditySku",commoditySku);
+
+        commoditySkuDao.update(map);
     }
 
     @Test
     public void del(){
-
+        Map<String,Object> map = new HashMap<>();
+        CommoditySku commoditySku = commoditySkuDao.findById(1);
+        commoditySku.setDel(1);
+        commoditySku.setSkuNo(15615);
+        map.put("CommoditySku",commoditySku);
+        int i = commoditySkuDao.del(map);
+        System.out.println(i);
     }
 }
